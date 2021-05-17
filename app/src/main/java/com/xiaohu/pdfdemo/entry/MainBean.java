f553ae4b1596b40f1523abdc9ddedb0d88af9080
yuanqiuhu <1916585396@qq.com>
2021-05-17 15:53:48 +0800

    第一次提交

---
+++ app/src/main/java/com/xiaohu/pdfdemo/entry/MainBean.java
@@ -0,0 +1,29 @@
+package com.xiaohu.pdfdemo.entry;
+
+
+public class MainBean {
+    /**
+     * 标题
+     */
+    private String title;
+    /**
+     * 描述
+     */
+    private String describe;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescribe() {
+        return describe;
+    }
+
+    public void setDescribe(String describe) {
+        this.describe = describe;
+    }
+}
